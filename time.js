60e44743c60bbc9dd2c77f122edb26be45136412
amcmorrow <andrew.michael.mcmorrow@gmail.com>
2018-04-18 17:54:20 -0500

    Update time.js

--- time.js
+++ time.js
@@ -1,6 +1,6 @@
   var today = new Date();
   var day = today.getDay();
-  var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+  var daylist = ["Sunray","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
   console.log("Today is : " + daylist[day] + ".");
   var hour = today.getHours();
   var minute = today.getMinutes();
